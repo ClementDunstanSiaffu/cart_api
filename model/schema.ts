8d8d6cd936d334e19e8d5c27f4c63be10b1f1dc0
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-10-25 10:17:06 +0300

    adding device id to get carts of the specific user  device

--- model/schema.ts
+++ model/schema.ts
@@ -2,23 +2,12 @@
 import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema({
-
-    id:{
-        type:String,
-    },
-    productName:{
-        type:String
-    },
-    productPrice:{
-        type:Number
-    },
-    productImage:{
-        type:String
-    },
-    available:{
-        type:String
-    }
-
+    id:{type:String},
+    deviceId:{type:String},
+    productName:{type:String},
+    productPrice:{type:Number},
+    productImage:{type:String},
+    available:{type:String}
 });
 
 mongoose.model("CARTSCHEMA",cartSchema);
